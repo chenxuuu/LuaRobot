22044a4da632d66372eafd9dacaccb2f0671d5f1
chenxuuu <lolicon@papapoi.com>
2019-11-04 10:40:45 +0800

    周末固定三人

--- appdata/lua/require/app/helpList.lua
+++ appdata/lua/require/app/helpList.lua
@@ -1,12 +1,15 @@
 --人员列表
 local members = {
-    290927815,
-    80005350,
-    1760580801,
-    1468647147,
-    124200583,
-    739657917,
-    389627265
+    1760580801,--盛
+    1468647147,--毅
+    124200583,--廖
+    961726194,--晨
+}
+
+local mo = {
+    290927815,--Delectate
+    80005350,--稀饭
+    389627265,--Wendal
 }
 
 return function (time)
@@ -14,9 +17,13 @@ return function (time)
     local day = t.yday - t.wday--取整，每周唯一的某一天
     math.randomseed(day+t.year*1000)--固定随机数种子，使结果保持一致性
 
+    table.insert(members,1,table.remove(mo,math.random(1,#mo)))--周日
+    table.insert(members,table.remove(mo,math.random(1,#mo)))--周六
+    table.insert(members,2,mo[1])--剩下的
+
     --打乱顺序
-    for i=1,#members do
-        local random = math.random(1,#members)
+    for i=2,#members-1 do
+        local random = math.random(2,#members-1)
         local temp = members[i]
         members[i] = members[random]
         members[random] = temp
