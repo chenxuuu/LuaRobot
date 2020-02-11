6bee16ded9096b7679409fd9ae5f0a1509028042
chenxuuu <lolicon@papapoi.com>
2020-02-11 11:19:11 +0800

    更正列表不对的bug

--- require/helpList.lua
+++ require/helpList.lua
@@ -1,11 +1,11 @@
 --人员列表
-local members = {
+local r_members = {
     1760580801,--盛
     1468647147,--毅
     124200583,--廖
 }
 
-local mo = {
+local r_mo = {
     290927815,--Delectate
     80005350,--稀饭
     389627265,--Wendal
@@ -17,6 +17,14 @@ return function (time)
     t.hour = 0 t.min = 0 t.sec = 0
     local ts = os.time(t) - t.wday*3600*24--取整，每周唯一的某一天
     math.randomseed(ts)--固定随机数种子，使结果保持一致性
+
+    local members,mo = {},{}
+    for i=1,#r_members do
+        members[i] = r_members[i]
+    end
+    for i=1,#r_mo do
+        mo[i] = r_mo[i]
+    end
 
     table.insert(members,1,table.remove(mo,math.random(1,#mo)))--周日
     table.insert(members,table.remove(mo,math.random(1,#mo)))--周六
