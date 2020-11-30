a73a9a655186cf832bc4b502f2ce964bb9382b3e
chenxuuu <lolicon@papapoi.com>
2020-11-30 15:30:45 +0800

    更正函数会被用户改的bug

--- sandbox/head.lua
+++ sandbox/head.lua
@@ -57,18 +57,22 @@ json = {
     end
 }
 
-local runCount = 0
-local start = os.time()
-local function trace (event, line)
-    runCount = runCount + 1
-    if runCount > 100000 then
-        error("运行代码量超过阈值")
+do
+    local runCount = 0
+    local start = os.time()
+    local ot = os.time
+    local e = error
+    local function trace (event, line)
+        runCount = runCount + 1
+        if runCount > 100000 then
+            e("运行代码量超过阈值")
+        end
+        if ot() - start >= 5 then
+            e("代码运行超时")
+        end
     end
-    if os.time() - start >= 5 then
-        error("代码运行超时")
+    debug.sethook(trace, "l")
     end
-end
-debug.sethook(trace, "l")
 
 loadstring = load
 
