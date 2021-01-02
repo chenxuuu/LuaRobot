6de1eef7ffd37dcff02cd38e8d1ccd9be77edc61
chenxuuu <lolicon@papapoi.com>
2021-01-02 16:49:50 +0800

    调整import顺序

--- main.lua
+++ main.lua
@@ -92,8 +92,7 @@ function asyncImage(url)
 end
 
 --加强随机数随机性
-import('System')
-math.randomseed(tostring(DateTime.Now.Ticks):reverse():sub(1, 6))
+math.randomseed(tostring(Utils.Ticks()):reverse():sub(1, 6))
 
 --获取随机字符串
 function getRandomString(len)
