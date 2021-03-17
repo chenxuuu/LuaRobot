7b0535aa248df099c9ac01b2cfd85f8c65b78255
chenxuuu <lolicon@papapoi.com>
2021-03-17 15:31:16 +0800

    加群答题检测改成一秒一次

--- events/GroupAddRequest.lua
+++ events/GroupAddRequest.lua
@@ -14,17 +14,22 @@ return function (data)
                 sys.wait(500)
             end
 
-            local a,b = math.random(0,100),math.random(0,100)
+            local a,b = math.random(0,10),math.random(0,10)
             local sent = cq.sendGroupMsg(data.group,cq.code.at(data.qq)..
             "欢迎加入本群，请在100秒内向我私聊发送答案，不然会被移出本群\r\n"..
                 "问题："..a.."+"..b.."等于多少？")
             XmlApi.Set("joinCheck",tostring(data.qq).."qq","wait")
-            sys.wait(120*1000)
+            local pass
+            for i=1,120 do
+                sys.wait(1000)
+                pass = tonumber(XmlApi.Get("joinCheck",tostring(data.qq).."qq")) == a+b
+                if pass then break end
+            end
             cq.deleteMsg(sent)
-            if tonumber(XmlApi.Get("joinCheck",tostring(data.qq).."qq")) ~= a+b then
-                cq.groupKick(data.group,data.qq,false)
-            else
+            if pass then
                 cq.groupBan(data.group,data.qq,0)
+            else
+                cq.groupKick(data.group,data.qq,false)
             end
             XmlApi.Delete("joinCheck",tostring(data.qq).."qq")
         end)
