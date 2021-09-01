72e72c2d28118662c523a08481be8bed92dd6916
chenxuuu <lolicon@papapoi.com>
2021-09-01 21:47:32 +0800

    提醒无意义群发

---
+++ require/apps/00repeat.lua
@@ -0,0 +1,30 @@
+
+local Groups = require("luat_groups")
+
+return {
+check = function (data)
+    for i=1,#Groups do
+        if Groups[i] == data.group then return true end
+    end
+end,
+run = function (data,sendMessage)
+    local last = XmlApi.Get("repeatCheck",tostring(data.qq))
+    if last == "" then
+        last = {}
+    else
+        last = jsonDecode(last)
+    end
+    for i=1,#last do
+        if last[i]["msg"] == data.msg and last[i]["group"] ~= data.group then
+            sendMessage(cq.code.at(data.qq).."\r\n您已经在别的群里发送过这条消息了，请不要重复发送。")
+            break
+        end
+    end
+    if #last >= 3 then
+        table.remove(last,1)
+    end
+    table.insert(last,{msg=data.msg,group=data.group})
+    last = jsonEncode(last)
+    XmlApi.Set("repeatCheck",tostring(data.qq),last)
+end
+}
